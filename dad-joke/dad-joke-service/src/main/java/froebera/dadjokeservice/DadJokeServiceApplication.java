73e3478d425477d64bdb84fe51ca8c245d550be7
Alexander Fröber <alex.froeber@gmail.com>
2019-10-29 10:07:13 +0100

    DadJokes logging & config

--- dad-joke/dad-joke-service/src/main/java/froebera/dadjokeservice/DadJokeServiceApplication.java
+++ dad-joke/dad-joke-service/src/main/java/froebera/dadjokeservice/DadJokeServiceApplication.java
@@ -1,5 +1,7 @@
 package froebera.dadjokeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,21 +9,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
 @SpringBootApplication
 public class DadJokeServiceApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(DadJokeServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(DadJokeServiceApplication.class, args);
-	}
-
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/joke").allowedOrigins("*");
-			}
-		};
+		logger.debug("Heyo");
 	}
 }
